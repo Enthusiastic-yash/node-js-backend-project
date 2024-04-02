612c59481a690cc98e16342088a732d724f64d5d
yash kashyap <yashkashyap1005@gmail.com>
2024-04-03 01:44:12 +0530

    add register controller and bug fixes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,5 +1,6 @@
 import multer from "multer";
 
+// multer is helping us to get images data from frontend  here first we save image on our temp file and then upload on cloudinary server
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './public/temp')
